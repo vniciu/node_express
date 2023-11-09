1d1737ab274603b985239aa697f8b312354cd371
Vinicius de Souza Vieira <vinicius.svieira8@gmail.com>
2023-11-09 07:59:51 -0300

    continuacao

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const { application } = require("express")
 const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const caminhoBase = path.join(__dirname, "templates")
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Estou funcionando com o express")
+    resposta.send(`${caminhoBase}/index.html`)
+
+})
+
+app.listen(3000,() =>{
+    console.log("Servidor rodando na porta 3000!")
 })
